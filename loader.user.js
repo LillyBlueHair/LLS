b746a351d0a0ec77f4e95462ca3b8f1ff7e7811d
LillyBlueHair <155645207+LillyBlueHair@users.noreply.github.com>
2025-05-29 16:05:24 +0200

    Create loader.user.js

---
+++ loader.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name BC Marking
+// @namespace https://www.bondageprojects.com/
+// @version 1.0-0
+// @description Lilly's Script Collection
+// @author LillyBlueHair
+// @downloadURL https://lillybluehair.github.io/LLS/loader.user.js
+// @updateURL https://lillybluehair.github.io/LLS/loader.user.js
+// @match https://*.bondageprojects.elementfx.com/R*/*
+// @match https://*.bondage-europe.com/R*/*
+// @match https://*.bondageprojects.com/R*/*
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const src = `https://lillybluehair.github.io/LLS/bundle.jsv=${Date.now()}`;
+    if (typeof LLS_loaded === "undefined") {
+        console.log("loading LLS");
+        const script = document.createElement("script");
+        script.src = src;
+        script.type = "text/javascript";
+        script.crossOrigin = "anonymous";
+        script.id = "LLS";
+        document.head.appendChild(script);
+    }
+})();
